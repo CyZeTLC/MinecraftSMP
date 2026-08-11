01aee1c122353de43379c1ae342ce128c95d1fc9
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-08-11 21:23:06 +0200

    chore: initial commit

---
+++ src/main/java/de/cyzetlc/smp/MinecraftSMP.java
@@ -0,0 +1,15 @@
+package de.cyzetlc.smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftSMP extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
